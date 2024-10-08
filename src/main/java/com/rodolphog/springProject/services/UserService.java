ec3019d3e95030c667c5805085070ba989899f86
Rodolpho Gomes <rodolpho.gda@gmail.com>
2024-10-08 13:02:41 -0300

    Service layer, component registration

---
+++ src/main/java/com/rodolphog/springProject/services/UserService.java
@@ -0,0 +1,29 @@
+package com.rodolphog.springProject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodolphog.springProject.entities.User;
+import com.rodolphog.springProject.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
