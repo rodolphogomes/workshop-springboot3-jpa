f3f5fd4395e87b0e757ffa849991c86345b3f692
Rodolpho Gomes <rodolpho.gda@gmail.com>
2024-10-10 13:06:03 -0300

    OrderStatus enum

--- src/main/java/com/rodolphog/springProject/config/TestConfig.java
+++ src/main/java/com/rodolphog/springProject/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.rodolphog.springProject.entities.Order;
 import com.rodolphog.springProject.entities.User;
+import com.rodolphog.springProject.entities.enums.OrderStatus;
 import com.rodolphog.springProject.repositories.OrderRepository;
 import com.rodolphog.springProject.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "99777777", "123456");
 		
 		
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),user1);
-		Order order2 = new Order(null, Instant.parse("2019-07-21T19:42:10Z"),user2);
-		Order order3 = new Order(null, Instant.parse("2019-07-22T19:21:22Z"),user1);
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,user1);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T19:42:10Z"), OrderStatus.WAITING_PAYMENT,user2);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T19:21:22Z"), OrderStatus.WAITING_PAYMENT,user1);
 		
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
