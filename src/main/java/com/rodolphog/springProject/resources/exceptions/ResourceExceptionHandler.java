e006e15352fcc731870e26551406bac4493be23b
Rodolpho Gomes <rodolpho.gda@gmail.com>
2024-10-23 09:48:28 -0300

    Exception handling - delete

--- src/main/java/com/rodolphog/springProject/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/rodolphog/springProject/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rodolphog.springProject.services.exceptions.DatabaseException;
 import com.rodolphog.springProject.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,4 +22,13 @@ public class ResourceExceptionHandler {
 		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> databaseException(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
+
